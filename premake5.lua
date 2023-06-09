f2ca1f117a24d4c420e408841dd7f2c69eb64925
JorgeSC <cruzjorgesalazar@gmail.com>
2023-06-09 17:08:04 -0600

    premake5 configuration for broadcast

--- premake5.lua
+++ premake5.lua
@@ -13,6 +13,7 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 group "Dependencies"
 	include "AudioChat/Dependencies/unp"
 	include "AudioChat/Dependencies/epoll-api"
+	include "AudioChat/Dependencies/broadcast"
 group ""
 
 group "Core"
