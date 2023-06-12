5a9c216700b0ff4372868661249fc80a8788bce1
JorgeSC <cruzjorgesalazar@gmail.com>
2023-06-12 12:15:40 -0600

    not working at all input/output

--- AudioChat/Dependencies/audio-api/main.c
+++ AudioChat/Dependencies/audio-api/main.c
@@ -1,6 +1,7 @@
 // #include <bits/stdc++.h>
 #include <portaudio.h>
 #include <stdio.h>
+#include <string.h>
 
 // using namespace std;
 
@@ -12,27 +13,21 @@ typedef struct {
     float right_phase;
 } paTestData;
 
-static int PACallback(const void *inputBuffer, const void *outputBuffer,
-                      unsigned long framesPerBuffer,
-                      const PaStreamCallbackTimeInfo *timeInfo,
-                      PaStreamCallbackFlags statusFlags, void *userData) {
-    /* Cast data passed through stream to our structure. */
-    paTestData *data = (paTestData *)userData;
-    float *out = (float *)outputBuffer;
-    unsigned int i;
-    (void)inputBuffer; /* Prevent unused variable warning. */
-    int cnt = 0;
-    for (i = 0; i < framesPerBuffer; i++) {
-        out[cnt++] = data->left_phase;  /* left */
-        out[cnt++] = data->right_phase; /* right */
-        /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
-        data->left_phase += 0.01f;
-        /* When signal reaches top, drop back down. */
-        if (data->left_phase >= 1.0f) data->left_phase -= 2.0f;
-        /* higher pitch so we can distinguish left and right. */
-        data->right_phase += 0.03f;
-        if (data->right_phase >= 1.0f) data->right_phase -= 2.0f;
-    }
+float gBuffer[256 * 4];
+
+static int PlaybackCallback(const void *inputBuffer, const void *outputBuffer,
+                            unsigned long framesPerBuffer,
+                            const PaStreamCallbackTimeInfo *timeInfo,
+                            PaStreamCallbackFlags statusFlags, void *userData) {
+    memcpy(outputBuffer, gBuffer, sizeof(float) * framesPerBuffer);
+    return 0;
+}
+
+static int CaptureCallback(const void *inputBuffer, const void *outputBuffer,
+                           unsigned long framesPerBuffer,
+                           const PaStreamCallbackTimeInfo *timeInfo,
+                           PaStreamCallbackFlags statusFlags, void *userData) {
+    memcpy(gBuffer, inputBuffer, sizeof(float) * framesPerBuffer);
     return 0;
 }
 
@@ -41,35 +36,57 @@ int main() {
     err = Pa_Initialize();
     if (err != paNoError) printf("PortAudio error: %s\n", Pa_GetErrorText(err));
 
-    PaStream *stream;
+    PaStream *playbackStream;
+    PaStream *captureStream;
     static paTestData data;
     data.left_phase = 0.0f;
     data.right_phase = 0.0f;
 
     /* Open an audio I/O stream. */
-    err = Pa_OpenDefaultStream(&stream, 0, /* no input channels */
-                               2,          /* stereo output */
-                               paFloat32,  /* 32 bit floating point output */
-                               SAMPLE_RATE,
-                               256,        /* frames per buffer, i.e. the number
-                                                  of sample frames that PortAudio will
-                                                  request from the callback. Many apps
-                                                  may want to use
-                                                  paFramesPerBufferUnspecified, which
-                                                  tells PortAudio to pick the best,
-                                                  possibly changing, buffer size.*/
-                               PACallback, /* this is your callback function */
-                               &data); /*This is a pointer that will be passed
-                                          to your callback*/
+    err = Pa_OpenDefaultStream(
+        &playbackStream, 0, /* no input channels */
+        1,                  /* stereo output */
+        paFloat32,          /* 32 bit floating point output */
+        SAMPLE_RATE, 256,   /* frames per buffer, i.e. the number
+                                   of sample frames that PortAudio will
+                                   request from the callback. Many apps
+                                   may want to use
+                                   paFramesPerBufferUnspecified, which
+                                   tells PortAudio to pick the best,
+                                   possibly changing, buffer size.*/
+        PlaybackCallback,   /* this is your callback function */
+        &data);             /*This is a pointer that will be passed
+                               to your callback*/
     if (err != paNoError) printf("PortAudio error: %s\n", Pa_GetErrorText(err));
 
-    err = Pa_StartStream(stream);
+    err = Pa_OpenDefaultStream(
+        &captureStream, 1, /* no input channels */
+        0,                 /* stereo output */
+        paFloat32,         /* 32 bit floating point output */
+        SAMPLE_RATE, 256,  /* frames per buffer, i.e. the number
+                                  of sample frames that PortAudio will
+                                  request from the callback. Many apps
+                                  may want to use
+                                  paFramesPerBufferUnspecified, which
+                                  tells PortAudio to pick the best,
+                                  possibly changing, buffer size.*/
+        CaptureCallback,   /* this is your callback function */
+        &data);            /*This is a pointer that will be passed
+                              to your callback*/
+    if (err != paNoError) printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+
+    err = Pa_StartStream(playbackStream);
+    if (err != paNoError) printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+
+    err = Pa_StartStream(captureStream);
     if (err != paNoError) printf("PortAudio error: %s\n", Pa_GetErrorText(err));
 
     /* Sleep for several seconds. */
     Pa_Sleep(NUM_SECONDS * 1000);
 
-    err = Pa_StopStream(stream);
+    err = Pa_StopStream(playbackStream);
+    if (err != paNoError) printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+    err = Pa_StopStream(captureStream);
     if (err != paNoError) printf("PortAudio error: %s\n", Pa_GetErrorText(err));
 
     err = Pa_Terminate();
