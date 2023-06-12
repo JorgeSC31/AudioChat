e714647d128151fceaae72c12d6e142532583c82
JorgeSC <cruzjorgesalazar@gmail.com>
2023-06-12 11:55:44 -0600

    premake5 files config

---
+++ AudioChat/Dependencies/audio-api/premake5.lua
@@ -0,0 +1,32 @@
+project "audio-api"
+	kind "ConsoleApp"
+	language "C"
+	-- cppdialect "C++17"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"*.h",
+		"*.c"
+	}
+
+	includedirs
+	{
+    }
+
+	links
+	{
+        "asound",
+        "m",
+		"portaudio"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
