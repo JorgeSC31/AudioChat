8df5fb10e830a88d7c0e5008ec7ad9de8e6f9618
JorgeSC <cruzjorgesalazar@gmail.com>
2023-06-13 00:27:22 -0600

    premake config

---
+++ AudioChat/Dependencies/config/premake5.lua
@@ -0,0 +1,29 @@
+project "config"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"*.h",
+		"*.cpp",
+	}
+
+	includedirs
+	{
+    }
+
+	links
+	{
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
